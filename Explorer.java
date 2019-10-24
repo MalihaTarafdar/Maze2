68f26bc7af8a09cb986364b0ff819c0c474339ff
Maliha Tarafdar <malihatarafdar@yahoo.com>
2019-10-24 08:09:40 -0400

    Update Explorer

--- Explorer.java
+++ Explorer.java
@@ -1,3 +1,57 @@
 public class Explorer {
-	
+
+	private Location loc;
+	private final int size;
+	private int dir; //0 EAST, 1 NORTH, 2 WEST, 3 SOUTH
+	private int health;
+
+	public enum Direction {
+		LEFT, RIGHT;
+	}
+
+	public Explorer(int row, int col, int size, int dir) {
+		loc = new Location(row, col);
+		this.size = size;
+		health = 100;
+	}
+
+	public void move(int dir, Structure[] maze) {
+		
+	}
+
+	public void turn(Direction turnDir) {
+		
+	}
+
+	public void setLoc(int row, int col) {
+		loc.setRow(row);
+		loc.setCol(col);
+	}
+	public Location getLoc() {
+		return loc;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public int getDir() {
+		return dir;
+	}
+
+	public void heal(int healAmount) {
+		health += healAmount;
+		if (health > 100) {
+			health = 100;
+		}
+	}
+	public void takeDamage(int damageAmount) {
+		health -= damageAmount;
+		if (health < 0) {
+			health = 0;
+		}
+	}
+	public int getHealth() {
+		return health;
+	}
 }
